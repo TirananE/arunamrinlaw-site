f5b3047ccb65cd9fe2ec0dce12b146e81efd3628
Tiranan Emson <tiranan.em@gmail.com>
2026-01-19 17:49:18 +0700

    chore: release-1.0.0 19-01-2026 : lazy load image

--- src/components/sections/BenefitSection.tsx
+++ src/components/sections/BenefitSection.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import React from 'react';
-import { useLanguage } from '../LanguageProvider';
+import React from "react";
+import Image from "next/image";
+import { useLanguage } from "../LanguageProvider";
 
 const BenefitSection = () => {
   const { t } = useLanguage();
@@ -11,34 +12,66 @@ const BenefitSection = () => {
         <div className="row">
           <div className="col-lg-5 col-md-12 col-sm-12 col-12">
             <div className="benefit_content" data-aos="fade-up">
-              <h6>{t('what_we_provide')}</h6>
-              <h2>{t('benefits_insurance')}</h2>
-              <p className="text text-size-18 mb-0">{t('benefits_insurance_desc')}</p>
+              <h6>{t("what_we_provide")}</h6>
+              <h2>{t("benefits_insurance")}</h2>
+              <p className="text text-size-18 mb-0">
+                {t("benefits_insurance_desc")}
+              </p>
             </div>
           </div>
           <div className="col-lg-7 col-md-12 col-sm-12 col-12">
-            <div className="benefit_wrapper position-relative" data-aos="fade-up">
+            <div
+              className="benefit_wrapper position-relative"
+              data-aos="fade-up"
+            >
               <ul className="list-unstyled mb-0">
                 <li className="beneft-box">
                   <figure className="icon">
-                    <img src="/assets/images/benefit-icon1.png" alt="image" className="img-fluid" />
+                    <Image
+                      src="/assets/images/benefit-icon1.png"
+                      alt="Complete insurance solutions"
+                      width={42}
+                      height={43}
+                      className="img-fluid"
+                      loading="lazy"
+                    />
                   </figure>
-                  <h5>{t('complete_insurance_solutions')}</h5>
-                  <p className="text-size-16 mb-0">{t('complete_insurance_solutions_desc')}</p>
+                  <h5>{t("complete_insurance_solutions")}</h5>
+                  <p className="text-size-16 mb-0">
+                    {t("complete_insurance_solutions_desc")}
+                  </p>
                 </li>
                 <li className="beneft-box">
                   <figure className="icon">
-                    <img src="/assets/images/benefit-icon2.png" alt="image" className="img-fluid" />
+                    <Image
+                      src="/assets/images/benefit-icon2.png"
+                      alt="Risk management solutions"
+                      width={42}
+                      height={43}
+                      className="img-fluid"
+                      loading="lazy"
+                    />
                   </figure>
-                  <h5>{t('risk_management_solutions')}</h5>
-                  <p className="text-size-16 mb-0">{t('risk_management_solutions_desc')}</p>
+                  <h5>{t("risk_management_solutions")}</h5>
+                  <p className="text-size-16 mb-0">
+                    {t("risk_management_solutions_desc")}
+                  </p>
                 </li>
                 <li className="beneft-box">
                   <figure className="icon">
-                    <img src="/assets/images/benefit-icon3.png" alt="image" className="img-fluid" />
+                    <Image
+                      src="/assets/images/benefit-icon3.png"
+                      alt="Claims management"
+                      width={42}
+                      height={43}
+                      className="img-fluid"
+                      loading="lazy"
+                    />
                   </figure>
-                  <h5>{t('claims_management')}</h5>
-                  <p className="text-size-16 mb-0">{t('claims_management_desc')}</p>
+                  <h5>{t("claims_management")}</h5>
+                  <p className="text-size-16 mb-0">
+                    {t("claims_management_desc")}
+                  </p>
                 </li>
               </ul>
             </div>
--- src/components/sections/FaqSection.tsx
+++ src/components/sections/FaqSection.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import Image from "next/image";
 
 const FaqSection = () => {
   return (
@@ -8,7 +9,14 @@ const FaqSection = () => {
           <div className="col-lg-6 col-md-12 col-sm-12 col-12">
             <div className="faq_wrapper position-relative" data-aos="zoom-in">
               <figure className="faq-image mb-0">
-                <img src="/assets/images/faq-image.jpg" alt="image" className="img-fluid" />
+                <Image
+                  src="/assets/images/faq-image.jpg"
+                  alt="Frequently asked questions"
+                  width={540}
+                  height={559}
+                  className="img-fluid"
+                  loading="lazy"
+                />
               </figure>
             </div>
           </div>
@@ -16,20 +24,40 @@ const FaqSection = () => {
             <div className="faq_content" data-aos="fade-up">
               <h6>Faq&apos;s</h6>
               <h2>Frequently Asked Questions</h2>
-              <p className="text text-size-16">Duisruam est qui dolorem ipsum quia dolor sit amer adipisci velit, sed nuia non numuam.</p>
+              <p className="text text-size-16">
+                Duisruam est qui dolorem ipsum quia dolor sit amer adipisci
+                velit, sed nuia non numuam.
+              </p>
               <div className="accordian-section-inner position-relative">
                 <div className="accordian-inner">
                   <div id="faq_accordion">
                     <div className="accordion-card">
                       <div className="card-header" id="faqheadingOne">
-                        <a href="#" className="btn btn-link collapsed" data-toggle="collapse" data-target="#faqcollapseOne" aria-expanded="false" aria-controls="faqcollapseOne">
-                          <h5 className="mb-0">Q: Can I customize my insurance plan?</h5>
+                        <a
+                          href="#"
+                          className="btn btn-link collapsed"
+                          data-toggle="collapse"
+                          data-target="#faqcollapseOne"
+                          aria-expanded="false"
+                          aria-controls="faqcollapseOne"
+                        >
+                          <h5 className="mb-0">
+                            Q: Can I customize my insurance plan?
+                          </h5>
                         </a>
                       </div>
-                      <div id="faqcollapseOne" className="collapse" aria-labelledby="faqheadingOne" data-parent="#faq_accordion">
+                      <div
+                        id="faqcollapseOne"
+                        className="collapse"
+                        aria-labelledby="faqheadingOne"
+                        data-parent="#faq_accordion"
+                      >
                         <div className="card-body">
-                          <p className="text-size-16 text-left mb-0">Our experienced insurance agents can help you assess your family&apos;s
-                            needs and recommend the appropriate coverage options based on factors such as your family size
+                          <p className="text-size-16 text-left mb-0">
+                            Our experienced insurance agents can help you assess
+                            your family&apos;s needs and recommend the
+                            appropriate coverage options based on factors such
+                            as your family size
                           </p>
                         </div>
                       </div>
